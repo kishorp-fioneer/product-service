a545f5ef384fe03aa0c1ab43cc8246c077fdab40
Kishor Patil <kishor.patil@fioneer.com>
2021-12-03 09:58:20 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+// ./test.js
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  duration: '1m',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+  },
+};
+
+export default function () {
+  const res = http.get('https://product-perf.c-94c5d0c.kyma.shoot.live.k8s-hana.ondemand.com/api/products/');
+  sleep(1);
+}

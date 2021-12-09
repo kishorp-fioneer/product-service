fad188d8d160e622b4adfa721ce3d1663a8460c2
Kishor Patil <kishor.patil@fioneer.com>
2021-12-09 12:25:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,31 +1,30 @@
 // ./test.js
 
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { Trend, Rate } from "k6/metrics";
+import { group, sleep } from "k6";
 
-export const options = {
-  stages: [
-    { duration: '2m', target: 100 }, // below normal load
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 300 }, // around the breaking point
-    { duration: '5m', target: 300 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
-  ],
+export let options = {
+    stages: [
+        { target: 10, duration: "60s" }
+    ]
 };
 
-export default function () {
-  const BASE_URL = 'https://test-api.k6.io'; // make sure this is not production
+var flow1RespTime = new Trend("flow_1_resp_time");
 
-  const responses = http.batch([
-    ['GET', `${BASE_URL}/public/crocodiles/1/`, null, { tags: { name: 'PublicCrocs' } }],
-    ['GET', `${BASE_URL}/public/crocodiles/2/`, null, { tags: { name: 'PublicCrocs' } }],
-    ['GET', `${BASE_URL}/public/crocodiles/3/`, null, { tags: { name: 'PublicCrocs' } }],
-    ['GET', `${BASE_URL}/public/crocodiles/4/`, null, { tags: { name: 'PublicCrocs' } }],
-  ]);
+export default function() {
+    group("Flow 1", function() {
+        let res = http.get("https://test.loadimpact.com/public/crocodiles/1/");
+        flow1RespTime.add(res.timings.duration);
+    });
+    group("Flow 2", function() {
+        let res = http.get("https://test.loadimpact.com/public/crocodiles/2/");
+        flow1RespTime.add(res.timings.duration);
+    });
+    group("Flow 3", function() {
+        let res = http.get("https://test.loadimpact.com/public/crocodiles/3/");
+        flow1RespTime.add(res.timings.duration);
+     });
+    sleep(1);
+};
 
-  sleep(1);
-}
